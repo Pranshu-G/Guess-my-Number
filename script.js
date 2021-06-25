4cad22a5f01e194fe97e3195b7562504b3adfd08
Pranshu Grover <pranshu.grover18@gmail.com>
2021-06-25 17:40:32 +0530

    scoring system

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 'use strict';
-console.log(document.querySelector('.message').textContent)
+/**console.log(document.querySelector('.message').textContent)
 document.querySelector('.message').textContent ='Correct Number 🔥';
 
 document.querySelector('.number').textContent =13
 document.querySelector('.score').textContent = 10;
+**/
 
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!'
+    }
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number'
+    }
+    else if(guess> secretNumber){
+        document.querySelector('.message').textContent = 'Too high';
+        score--;
+        document.querySelector('.score').textContent = score;
+
+
+    }else if(guess<secretNumber){
+        document.querySelector('.message').textContent = 'Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+});
